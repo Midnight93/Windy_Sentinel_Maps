08664af246d49e6971246cc37df8acf4fab5d6cc
Edoardo Piccari <edoardopiccari@gmail.com>
2021-05-11 16:25:51 +0200

    Creazione file JSON file all'output

--- post_request.py
+++ post_request.py
@@ -11,7 +11,7 @@ parser.add_argument(
           required=True,
           help="Your API Keys",
       )
-      
+
 parser.add_argument(
           "--lat",
           type=str,
@@ -32,6 +32,13 @@ parser.add_argument(
           help="Reference model",
       )
 
+parser.add_argument(
+          "--output",
+          type=str,
+          required=False,
+          help="Define your output file name ex. output.json",
+      )
+
 if len(sys.argv[1:]) == 0:
      parser.print_help()
      parser.exit()
@@ -47,7 +54,7 @@ source_code = {
       'lon': args.lon,
       'model': args.model,
       'parameters': ['wind', 'dewpoint', 'rh', 'pressure'],
-      'levels': ['surface', '800h', '300h'], 
+      'levels': ['surface', '800h', '300h'],
       'key': args.api_key
       }
 
@@ -56,5 +63,11 @@ headers = {'content-type': 'application/json'}
 r = requests.post(url = API_ENDPOINT, data=json.dumps(source_code), headers=headers)
 
 # Leggo risposta
-pastebin_url = r.text
-print("Responso del server:"+"  "+pastebin_url)
+risposta = r.text
+lettura_json = json.loads(risposta)
+risposta_formatted_str = json.dumps(lettura_json, indent=4)
+print(risposta_formatted_str)
+
+if args.output is not None:
+    with open(args.output, 'w') as outfile:
+        json.dump(lettura_json,outfile,indent=4)
